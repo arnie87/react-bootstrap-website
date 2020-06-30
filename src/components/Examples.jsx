f96b4742d92bd9703987681696ba406caa3f3bbf
arnie87 <arnie87@users.noreply.github.com>
2020-06-30 11:33:47 +0100

    Update Examples.jsx

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -12,7 +12,7 @@ export default class Examples extends Component {
           <Col xs={12} sm={8} smOffset={2}>
             <Image src="assets/form.png" className="example-pic" rounded />
             <h3>Form with validation</h3>
-            <Link to="/">
+            <Link to="https://cranky-brown-916f65.netlify.com/">
               <Button bsStyle="primary">Open Example</Button>
               <Button bsStyle="link">GitHub</Button>
             </Link>
